f8b622eeff9220f3b45b20bd6928274c4c0e471a
reuben4046 <poolereuben@gmail.com>
2024-11-13 10:59:26 +1100

    chunk

--- Assets/Reuben/Scripts/EndlessLevel/ChunkSpawner.cs
+++ Assets/Reuben/Scripts/EndlessLevel/ChunkSpawner.cs
@@ -4,23 +4,32 @@ using UnityEngine;
 
 public class ChunkSpawner : MonoBehaviour
 {
+    public static ChunkSpawner instance;
+
     [SerializeField] private Transform player;
 
     [SerializeField] private int chunkSize;
 
     private float chunkCenter;
+    [SerializeField] private GameObject[] chunks;
 
+    void Awake()
+    {
+        instance = this;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         chunkCenter = chunkSize / 2;
+        GenerateChunk();
     }
 
-    // Update is called once per frame
-    void Update()
+    void GenerateChunk()
     {
-        
+        int randomChunk = Random.Range(0, chunks.Length);
+        GameObject chunk = Instantiate(chunks[randomChunk], new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        chunk.transform.parent = transform;
     }
 
     void OnDrawGizmos()

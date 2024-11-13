2f04575e1a5575487a5b2599f4138c453c36ad2f
reuben4046 <poolereuben@gmail.com>
2024-11-13 14:05:39 +1100

    Got the endless chunk system working

--- Assets/Reuben/Scripts/EndlessLevel/ChunkSpawner.cs
+++ Assets/Reuben/Scripts/EndlessLevel/ChunkSpawner.cs
@@ -1,52 +1,66 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class ChunkSpawner : MonoBehaviour
 {
-    public static ChunkSpawner instance;
-
     [SerializeField] private Transform player;
 
     [SerializeField] private int chunkSize;
 
     private float chunkCenter;
 
-    List<Chunk> chunks = new List<Chunk>();
+    [SerializeField] private List<Chunk> chunks = new List<Chunk>();
+    private List<Chunk> chunksInScene = new List<Chunk>();
+    
+    Chunk mostRecentChunk;
 
     void Awake()
     {
-        instance = this;
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
         chunkCenter = chunkSize / 2;
-        GenerateChunk();
+        GenerateChunk(transform.position.x);
     }
 
-    void GenerateChunk()
+    void Update()
+    {
+        SpawnChunks();
+        DestroyOldChunks();
+       //Debug.Log(chunksInScene.Count);
+    }
+
+    void SpawnChunks()
+    {
+        if (player.position.x > mostRecentChunk.transform.position.x)
+        {
+            float spawnPositionX = mostRecentChunk.transform.position.x + chunkSize;
+            GenerateChunk(spawnPositionX);
+        }
+    }
+
+    void GenerateChunk(float positionX)
     {
         int randomChunk = Random.Range(0, chunks.Count);
-        Chunk chunk = Instantiate(chunks[randomChunk], new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
-        chunk.transform.parent = transform;
+        mostRecentChunk = Instantiate(chunks[randomChunk], new Vector2(positionX, transform.position.y), Quaternion.identity);
+        mostRecentChunk.transform.localScale = new Vector2(chunkSize, chunkSize);
+        mostRecentChunk.transform.parent = transform;
+
         foreach (Chunk c in chunks)
         {
-            if (c != chunk)
-            {
-                c.isMostRecentChunk = false;
-            }
+            c.isMostRecentChunk = false;
         }
 
+        mostRecentChunk.isMostRecentChunk = true;
+        chunksInScene.Add(mostRecentChunk);
     }
 
-    void OnDrawGizmos()
+    void DestroyOldChunks()
     {
-        Gizmos.color = Color.green;
-
-        Gizmos.DrawWireCube(transform.position, new Vector2(chunkSize, chunkSize));
+        if (chunksInScene.Count > 4)
+        {
+            Chunk oldestChunk = chunksInScene[0];
+            chunksInScene.Remove(oldestChunk);
+            Destroy(oldestChunk.gameObject);
+        }
     }
+
 }
 

d6a857a638125266090347d318a485cc43823c82
reuben4046 <93184269+reuben4046@users.noreply.github.com>
2024-11-13 17:20:14 +1100

    commint

--- Assets/Reuben/Scripts/EndlessLevel/Chunk.cs
+++ Assets/Reuben/Scripts/EndlessLevel/Chunk.cs
@@ -5,4 +5,22 @@ using UnityEngine;
 public class Chunk : MonoBehaviour
 {
     public bool isMostRecentChunk = true;
+
+    [SerializeField] private float chunkCentre;
+
+    Vector2 pos;
+    Vector2 pos2;
+
+    private void Start()
+    {
+        pos = new Vector2(transform.localScale.x/2f, -0.2f);
+        pos2 = new Vector2(transform.localScale.x, -0.2f);
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(pos, pos);
+    }
 }
